622310b0da278f7eecb485f6c4e17db9c01936ed
lfborjas <luis.borjas@escolarea.com>
2011-04-06 19:13:40 -0600

    listas funciones de orden superior

--- parcial1/semana1/sesion2/higher_order_fun.py
+++ parcial1/semana1/sesion2/higher_order_fun.py
@@ -31,6 +31,80 @@ def quicksort(l, key=lambda x:x):
                quicksort([e for e in tail if key(e) >= key(head)], key)
 
 
+def compose_3(f,g,h):
+    """
+    Composición de funciones: devuelve una función que aplica las tres originales, en cadena,
+    a un argumento.
+    Nótese que, en la segunda prueba, estamos llamando inmediatamente a la función que se nos
+    retorna.
+
+    >>> f = compose_3(str.split, str.title, lambda s: s.replace("_", " "))
+    >>> f("hola_mundo")
+    ['Hola', 'Mundo']
+    >>> compose_3(str.split, str.title, lambda s: s.replace("_", " "))("hola_mundo")
+    ['Hola', 'Mundo']
+    """
+    def composed(x):
+        return f(g(h(x)))
+    return composed
+
+def compose(*funs):
+    """
+    Composición en el caso general: aplica todas las funciones en cadena
+    desde la última a la primera
+
+    ¿Qué hace la función predefinida ``reduce``?
+
+    >>> import operator as op
+    >>> compose(op.invert, op.neg)(666)
+    665
+    >>> compose(lambda x: x << 2, lambda x: x % 2, lambda x: x**2)(365)
+    4
+    """
+    return lambda x: reduce(lambda acum, f: f(acum), funs[::-1], x)
+
+
+def partial(f, *preloaded_args, **preloaded_kwargs):
+    """
+    Aplicación parcial de funciones
+
+    retorna una función que acepta los argumentos que le "faltan" a la original
+    >>> sum3 = lambda x,y,z: x+y+z
+    >>> sum3(1,2,3)
+    6
+    >>> sum3_with_1 = partial(sum3, 1)
+    >>> sum3_with_1(2,3)
+    6
+    >>> sorted("mundo hola algo".split(), key=len)
+    ['hola', 'algo', 'mundo']
+    >>> sort_by_len = partial(sorted, key=len)
+    >>> sort_by_len("mundo hola algo".split())
+    ['hola', 'algo', 'mundo']
+    """
+    
+    def preloaded(*args, **kwargs):
+        #¿Qué hace la función update de la clase dict?
+        kwargs.update(**preloaded_kwargs)
+        return f(*(preloaded_args+args), **kwargs)
+
+    return preloaded
+
+def flip(f):
+    """
+    Función que envía sus parámetros al revés a la original
+
+    >>> import operator as op
+    >>> op.lshift(8,2)
+    32
+    >>> shiftl = flip(op.lshift)
+    >>> shiftl(8,2)
+    512
+    """
+
+    def inverted(*args):
+        return f(*(args[::-1]))
+
+    return inverted
 
 if __name__ == "__main__":
     import doctest
